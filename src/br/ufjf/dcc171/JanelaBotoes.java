1e1c934fcb18532b41e2ff7a79b87f41cb82bfc2
ice <ice@localhost>
2017-08-17 22:23:24 -0300

    Aula04 - Botões (Contadores adicionados)

--- src/br/ufjf/dcc171/JanelaBotoes.java
+++ src/br/ufjf/dcc171/JanelaBotoes.java
@@ -13,6 +13,8 @@ class JanelaBotoes extends JFrame {
 
     private final JButton btn1;
     private final JButton btn2;
+    private int b1 = 0;
+    private int b2 = 0;
 
     public JanelaBotoes() throws HeadlessException {
         super("Botões");
@@ -24,8 +26,10 @@ class JanelaBotoes extends JFrame {
         btn1.setRolloverIcon(new ImageIcon("resources/ufjf.gif"));
         add(btn2);
 
-        btn1.addActionListener(new BotaoClicado());
-        btn2.addActionListener(new BotaoClicado());
+        BotaoClicado bc = new BotaoClicado();
+
+        btn1.addActionListener(bc);
+        btn2.addActionListener(bc);
     }
 
     private class BotaoClicado implements ActionListener {
@@ -33,9 +37,11 @@ class JanelaBotoes extends JFrame {
         @Override
         public void actionPerformed(ActionEvent e) {
             if (e.getSource() == btn1) {
-                JOptionPane.showMessageDialog(null, "Você clicou o botão com imagem", "Mensagem", JOptionPane.INFORMATION_MESSAGE);
+                b1++;
+                JOptionPane.showMessageDialog(null, "Gol da Alemanha\nAlemanha " + b1 + " X Brasil " + b2, "Mensagem", JOptionPane.INFORMATION_MESSAGE);
             } else {
-                JOptionPane.showMessageDialog(null, "Você clicou o botão sem imagem", "Mensagem", JOptionPane.INFORMATION_MESSAGE);
+                b2++;
+                JOptionPane.showMessageDialog(null, "Olha a bola tocada!! Virou Passeio!!\nAlemanha " + b1 + " X Brasil " + b2, "Mensagem", JOptionPane.INFORMATION_MESSAGE);
             }
         }
 
